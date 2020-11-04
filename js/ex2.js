e6ad1df5c96fc0b735ae7263f867c1f33a527f2a
wallacy <wwallacy@Live.com>
2020-11-04 17:30:12 -0300

    Terceiro commit

---
+++ js/ex2.js
@@ -0,0 +1,14 @@
+document.querySelector("#btn").addEventListener('click', () =>{
+    let n1,n2,total;
+    n1 = parseInt(document.getElementById("n1").value); /* com parseint 1 sem parseint '1' */
+    n2 = parseInt(document.getElementById("n2").value);
+    /* toda as vezes que a informação vier de um input ela vem como se fosse uma strin '1' */
+
+    total = n1+n2;
+    alert('Resultado= '+total);
+    /* nan - not a number = nao e um numero */
+    /* acrecentar os calculos de subtração n1 - n2, multiplicação n1*N2 e divisao
+    porem tome cuidado pois o usuario pode digitar 0 e eu não posso dividir por 0.
+    n1/n2 se n2>0
+    mostrar os resuldatados no mesmo alert, somente quebrando a linha */
+})
